a3012fb6cbb95abf56264a6ec3a5d6f193840bf2
dredizzle <dredizzle@live.co.uk>
2019-12-30 18:10:33 +0000

    user login validation and authorisation

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyDhluviLh3bEHBlS8TfsWXABfsoSefAmcE',
+  authDomain: 'social-media-app-5af76.firebaseapp.com',
+  databaseURL: 'https://social-media-app-5af76.firebaseio.com',
+  projectId: 'social-media-app-5af76',
+  storageBucket: 'social-media-app-5af76.appspot.com',
+  messagingSenderId: '901034815058',
+  appId: '1:901034815058:web:8381d2c119a5f1d00bb98b'
+}
